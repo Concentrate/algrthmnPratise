b66728afa2ce5b025d8ecf0ced2179bbbf7116b7
liudeyu <liudeyu@baidu.com>
2017-04-09 15:48:39 +0800

    加深对动态规划的理解，加入两道例题，从回溯，回溯带备忘录，普通动态规划，动态规划带空间优化来思考

---
+++ src/AlgrithmnPratise/dynamicProgram/ClosestMinDistrubtion.java
@@ -0,0 +1,130 @@
+package AlgrithmnPratise.dynamicProgram;
+
+import java.util.Scanner;
+
+/**
+ * Created by liudeyu on 2017/4/9.
+ */
+/*
+题目背景：
+现在有一袋硬币，里面最多有100个硬币，面值区间为[1, 500]，要分给两个人，并使得他们所获得的金钱总额之差最小，并给出这个最小差值。
+* */
+public class ClosestMinDistrubtion {
+    int total;
+    int[] value;
+    int minDifference = Integer.MAX_VALUE;
+    int[][] memo;
+
+    void initMemoSpace(int total, int n) {
+        memo = new int[n+1][];
+        for (int i = 0; i <= n; i++) {
+            memo[i] = new int[total + 1];
+        }
+    }
+
+
+    int minGapDistrubution(int step, int disOneValue) {
+        if (step >= value.length) {
+            if (Math.abs(total - disOneValue - disOneValue) < minDifference) {
+                minDifference = Math.abs(total - disOneValue - disOneValue);
+            }
+            return Math.abs(total - disOneValue - disOneValue);
+        }
+        int ans = minGapDistrubution(step + 1, disOneValue + value[step]);
+        int ans1 = minGapDistrubution(step + 1, disOneValue);
+        return Math.min(ans, ans1);
+    }
+
+
+    int recursiveWithMemo(int step, int disOneValue) {
+        memo[step][disOneValue] = Math.abs(total - disOneValue - disOneValue);
+        if (step >= value.length) {
+            if (Math.abs(total - disOneValue - disOneValue) < minDifference) {
+                minDifference = Math.abs(total - disOneValue - disOneValue);
+            }
+            return Math.abs(total - disOneValue - disOneValue);
+        }
+        int an, an1;
+        if (memo[step + 1][disOneValue + value[step]] != 0) {
+            an = memo[step + 1][disOneValue + value[step]];
+        } else {
+            an = recursiveWithMemo(step + 1, disOneValue + value[step]);
+        }
+        if (memo[step + 1][disOneValue] != 0) {
+            an1 = memo[step + 1][disOneValue];
+        } else {
+            an1 = recursiveWithMemo(step + 1, disOneValue);
+        }
+        return Math.min(an, an1);
+    }
+
+
+    /*动态规划自底向上的方式*/
+    int minGapDitributionWithDynamicProgram(){
+        int num=value.length;
+        int [][]V=new int[num+1][];
+        for(int i=0;i<num+1;i++){
+            V[i]=new int[total+1];
+        }
+        for(int i=0;i<=total;i++){
+            V[0][i]=Math.abs(total-i-i);
+        }
+        for(int i=0;i<=num;i++){
+            V[i][0]=total;
+        }
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=total;j++){
+               if(j>=value[i-1]){
+                   V[i][j]=Math.min(V[i-1][j-value[i-1]],V[i-1][j]);
+               }else{
+                   V[i][j]=V[i-1][j];
+               }
+            }
+        }
+        return V[num][total];
+    }
+
+
+    /**动态规划，自底向上空间优化*/
+    int minGapDitributionDynamicWithSpaceOptime(){
+        int n=value.length;
+        int []V=new int[total+1];
+        for(int i=0;i<=total;i++){
+            V[i]=Math.abs(total-i-i);
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=total;j>=value[i-1];j--){
+                V[j]=Math.min(V[j-value[i-1]],V[j]);
+            }
+        }
+        return V[total];
+    }
+
+
+
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        int[] array = new int[n];
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+//            array[i] = scanner.nextInt();
+//            array[i]= (int) (Math.random()*n);
+            array[i]=i;
+            total += array[i];
+        }
+        ClosestMinDistrubtion minDistrubtion = new ClosestMinDistrubtion();
+        minDistrubtion.value = array;
+        minDistrubtion.total = total;
+//        minDistrubtion.initMemoSpace(total,n);
+        long startTime=System.currentTimeMillis();
+//        int a = minDistrubtion.minGapDistrubution(0, 0);
+//        int a=minDistrubtion.recursiveWithMemo(0,0);
+//        int a=minDistrubtion.minGapDitributionWithDynamicProgram();
+        int a=minDistrubtion.minGapDitributionDynamicWithSpaceOptime();
+        long endTime=System.currentTimeMillis();
+        System.out.println("The min gap is " + a+" and use time is "+(endTime-startTime)+" ms");
+
+    }
+}
---
+++ src/AlgrithmnPratise/dynamicProgram/MinChangeToalMoney.java
@@ -0,0 +1,123 @@
+package AlgrithmnPratise.dynamicProgram;
+
+import java.util.Scanner;
+
+/**
+ * Created by liudeyu on 2017/4/9.
+ */
+public class MinChangeToalMoney {
+    /*如果我们有面值为1元、3元和5元的硬币若干枚，如何用最少的硬币凑够11元？ */
+    int[] money;
+    int changeMoney;
+    int minNumber;
+    int num;
+
+    public MinChangeToalMoney(int[] money, int changeMoney) {
+        this.money = money;
+        this.changeMoney = changeMoney;
+        num = money.length;
+        minNumber = Integer.MAX_VALUE;
+    }
+
+    /*还是从最容易想到的回溯法想起*/
+    public int recursiveGetMinNumberWithoutMemo(int curMoney, int curPosition, int mNumber) {
+
+
+        if (curPosition >= num) {
+            if (curMoney == changeMoney) {
+                return mNumber;
+            }
+            return Integer.MAX_VALUE;
+        }
+        if (curMoney == changeMoney) {
+            if (mNumber > minNumber) {
+                minNumber = mNumber;
+            }
+            return mNumber;
+        }
+
+        int an1 = recursiveGetMinNumberWithoutMemo(curMoney + money[curPosition], curPosition + 1, mNumber + 1);
+        int an2 = recursiveGetMinNumberWithoutMemo(curMoney, curPosition + 1, mNumber);
+        return Math.min(an1, an2);
+    }
+
+    /* recursive agrithmn with memo*/
+    public int recursiveGetMinNumberWithMemo() {
+        int[][] memo = new int[num + 1][];
+        for (int i = 0; i <= num; i++) {
+            memo[i] = new int[changeMoney + 1];
+        }
+
+        return toImplementRecursiveWithMemo(memo, 0, 0, 0);
+    }
+    // todo 算法不对，待思考和修改，自顶向下带备忘录的recursive 结果不对
+    private int toImplementRecursiveWithMemo(int[][] memo, int position, int curMoney, int coinNumber) {
+        if (position >= num||curMoney>changeMoney) {
+            if (curMoney == changeMoney) {
+                return coinNumber;
+            }
+            return Integer.MAX_VALUE;
+        }
+        memo[position][curMoney] = coinNumber;
+        if (curMoney == changeMoney) {
+            if (coinNumber < minNumber) {
+                minNumber = coinNumber;
+            }
+            return coinNumber;
+        }
+
+        int an, an1;
+        if (curMoney+money[position]<=changeMoney&&memo[position + 1][curMoney + money[position]] != 0) {
+            an = memo[position + 1][curMoney + money[position]];
+        } else {
+            an = toImplementRecursiveWithMemo(memo, position + 1, curMoney + money[position], coinNumber + 1);
+        }
+        if (position+1<num&&memo[position + 1][curMoney] != 0) {
+            an1 = memo[position + 1][curMoney];
+        } else {
+            an1 = toImplementRecursiveWithMemo(memo, position + 1, curMoney, coinNumber);
+        }
+        return Math.min(an, an1);
+
+    }
+
+
+    /*动态规划，自底向上的思考*/
+    int getMinNumOfChangeWithDynamicPragramAndSpaceOptime(){
+        int []V=new int[changeMoney+1];
+        for(int i=0;i<=changeMoney;i++){
+            V[i]= 0;
+            if(i!=0){
+                V[i]=Integer.MAX_VALUE-10;
+            }
+        }
+        for(int i=1;i<=num;i++){
+            for(int j=changeMoney;j>=money[i-1];j--){
+                V[j]=Math.min(V[j-money[i-1]]+1,V[j]);
+            }
+        }
+        return V[changeMoney];
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+//            array[i]=scanner.nextInt();
+            array[i] = i;
+        }
+//        int totalMoney=scanner.nextInt();
+        int totalMoney = (0 + n - 1) * n / 2;
+        MinChangeToalMoney changeToalMoney = new MinChangeToalMoney(array, totalMoney);
+        long startTime = System.currentTimeMillis();
+//        int minNumber = changeToalMoney.recursiveGetMinNumberWithoutMemo(0, 0, 0);
+//        int minNumber = changeToalMoney.recursiveGetMinNumberWithMemo();
+        int minNumber=changeToalMoney.getMinNumOfChangeWithDynamicPragramAndSpaceOptime();
+        long endTime = System.currentTimeMillis();
+        System.out.println("The min number is " + minNumber + " and use time is " + (endTime - startTime) + " ms");
+
+
+    }
+}
